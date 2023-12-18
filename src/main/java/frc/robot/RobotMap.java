26a6df00f9956c177f23e97d851b675cbb797e8b
AryaVaidya08 <aryav663@gmail.com>
2023-12-18 13:52:38 -0500

    add rotations

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,7 +6,7 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
-
+import edu.wpi.first.math.util.Units;
 
 public final class RobotMap {
 
@@ -37,11 +37,11 @@ public final class RobotMap {
 		public static final double DOUBLE_SUBSTATION_TARGET_HEIGHT_METERS = 0.59;
 		public static final double CAMERA_PITCH_RADIANS = 0; // NOT FINAL
 		
-		public static final Transform3d ROBOT_TO_FRONT_CAMERA= new Transform3d(new Translation3d(0.406, 0, FRONT_CAMERA_HEIGHT_METERS),new Rotation3d());
-		public static final Transform3d ROBOT_TO_BACK_CAMERA= new Transform3d(new Translation3d(0.4571,0,BACK_CAMERA_HEIGHT_METERS),new Rotation3d());
+		public static final Transform3d ROBOT_TO_FRONT_CAMERA= new Transform3d(new Translation3d(0.406, 0, FRONT_CAMERA_HEIGHT_METERS),new Rotation3d(0.0,0.0,0.0));
+		public static final Transform3d ROBOT_TO_BACK_CAMERA= new Transform3d(new Translation3d(0.4571,0,BACK_CAMERA_HEIGHT_METERS),new Rotation3d(0,Units.degreesToRadians(180),0));
 		//TODO: Measuring
-		public static final Transform3d ROBOT_TO_LEFT_CAMERA= new Transform3d(new Translation3d(0.4571,0,BACK_CAMERA_HEIGHT_METERS),new Rotation3d());
-		public static final Transform3d ROBOT_TO_RIGHT_CAMERA= new Transform3d(new Translation3d(0.4571,0,BACK_CAMERA_HEIGHT_METERS),new Rotation3d());
+		public static final Transform3d ROBOT_TO_LEFT_CAMERA= new Transform3d(new Translation3d(0,0.2794,LEFT_CAMERA_HEIGHT_METERS),new Rotation3d(0,Units.degreesToRadians(90),0));
+		public static final Transform3d ROBOT_TO_RIGHT_CAMERA= new Transform3d(new Translation3d(0,0.2794,RIGHT_CAMERA_HEIGHT_METERS),new Rotation3d(0,Units.degreesToRadians(270),0));
 
 	}
 	public static final class DDrive {
